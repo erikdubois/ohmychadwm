53f3fa217a0dedf0ecb695f21644de99436fe72b
Erik Dubois <erik.dubois@gmail.com>
2026-03-29 08:16:00 +0200

    update

--- up.sh
+++ up.sh
@@ -35,12 +35,7 @@
 
 git add --all .
 
-# Give a comment to the commit if you want
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-
-read input
+input="update"
 
 # Committing to the local repository with a message containing the time details and commit text
 
