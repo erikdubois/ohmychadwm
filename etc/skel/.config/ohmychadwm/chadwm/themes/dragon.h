c321a6ff39fbb38bfc2c3d04201e1bcc45f1aeaf
Erik Dubois <erik.dubois@gmail.com>
2026-04-06 13:53:41 +0200

    update

--- etc/skel/.config/ohmychadwm/chadwm/themes/dragon.h
+++ etc/skel/.config/ohmychadwm/chadwm/themes/dragon.h
@@ -4,10 +4,10 @@
 #define THEME_AUTOHIDE    1
 #define THEME_SHOWSYSTRAY 1
 #define THEME_BORDER      5
-#define THEME_SMARTGAPS 
+#define THEME_SMARTGAPS 0
 #define THEME_MFACT   0.50
 #define THEME_NMASTER 1
-#define THEME_FONT    "JetBrainsMono Nerd Font Mono:style:bold:size=13"  0
+#define THEME_FONT    "JetBrainsMono Nerd Font Mono:style:bold:size=13"
 
 static const char col_borderbar[]      = "#361647";
 
