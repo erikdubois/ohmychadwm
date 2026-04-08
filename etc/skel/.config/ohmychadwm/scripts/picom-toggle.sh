bbf9caafc833d63b610a932110fb027ce2238883
Erik Dubois <erik.dubois@gmail.com>
2026-04-08 06:25:00 +0200

    update

--- etc/skel/.config/ohmychadwm/scripts/picom-toggle.sh
+++ etc/skel/.config/ohmychadwm/scripts/picom-toggle.sh
@@ -5,17 +5,24 @@ CONFIG_DIR="$HOME/.config/ohmychadwm/picom"
 if pgrep -x "picom" > /dev/null; then
     notify-send "Picom" "Stopping picom..."
     killall picom
-else
-    # Get list of .conf files (basename only)
-    options=$(basename -a "$CONFIG_DIR"/*.conf)
-    
-    # Use rofi to prompt the user
-    chosen=$(echo "$options" | rofi -dmenu -p "Choose picom config:")
+    exit 0
+fi
 
-    if [[ -n "$chosen" && -f "$CONFIG_DIR/$chosen" ]]; then
-        notify-send "Picom" "Starting picom with: $chosen"
-        picom -b --config "$CONFIG_DIR/$chosen" &
-    else
-        notify-send "Picom" "No valid selection made."
-    fi
+# kill fastcompmgr if running before starting picom
+if pgrep -x "fastcompmgr" > /dev/null; then
+    killall fastcompmgr
+    sleep 0.3
+fi
+
+# Get list of .conf files (basename only)
+options=$(basename -a "$CONFIG_DIR"/*.conf)
+
+# Use rofi to prompt the user
+chosen=$(echo "$options" | rofi -dmenu -p "Choose picom config:")
+
+if [[ -n "$chosen" && -f "$CONFIG_DIR/$chosen" ]]; then
+    notify-send "Picom" "Starting picom with: $chosen"
+    picom -b --config "$CONFIG_DIR/$chosen" &
+else
+    notify-send "Picom" "No valid selection made."
 fi
